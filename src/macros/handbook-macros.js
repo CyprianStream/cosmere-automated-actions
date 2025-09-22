c26a944280ec5b70b5b4e15d5093c8004d8045e3
Khunkurisu <creative.3d@live.com>
2025-09-22 16:12:42 -0500

    chore: remove brackets from macro calls
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/macros/handbook-macros.js
+++ src/macros/handbook-macros.js
@@ -43,9 +43,9 @@ import { progression } from "./surges/progression/progression.js"
 
 export const handbookMacros = {
     //common stormlight actions
-    breathe_stormlight: breatheStormlight(),
-    enhance: enhance(),
-    stormlight_reclamation: stormlightReclamation(),
+    breathe_stormlight: breatheStormlight,
+    enhance: enhance,
+    stormlight_reclamation: stormlightReclamation,
 
     //Abrasion actions
 
@@ -66,10 +66,10 @@ export const handbookMacros = {
 
 
     //Progression actions
-    explosive_growth: explosiveGrowth(),
-    from_the_brink: fromTheBrink(),
-    injury_regrowth: injuryRegrowth(),
-    progression: progression()
+    explosive_growth: explosiveGrowth,
+    from_the_brink: fromTheBrink,
+    injury_regrowth: injuryRegrowth,
+    progression: progression,
 
     //Tension actions
 
@@ -78,4 +78,4 @@ export const handbookMacros = {
 
 
     //Transportation actions
-} 
+}
