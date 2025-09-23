d02eabf857dc632598abcbcc0c3a1280931784af
Khunkurisu <creative.3d@live.com>
2025-09-23 02:41:29 -0500

    feat: use new macro object
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-import { handbookMacros } from "./macros/handbook-macros.js";
-import { worldguideMacros } from "./macros/worldguide-macros.js";
-import { starterRulesMacros } from "./macros/starter-rules-macros.js"
+import { macros } from "./macros/index.js";
 import { registerModuleSettings } from "./utils/settings.js";
 import { OFFICIAL_MODULES } from "./utils/constants.js";
 import { IsModuleActive } from "./utils/helpers.js";
 
 Hooks.once('init', () => {
 	globalThis.cosmereAutomatedActions = {
-		handbookMacros,
-		starterRulesMacros,
-		worldguideMacros
+		macros,
 	}
 
 	registerModuleSettings();
@@ -28,20 +24,9 @@ Hooks.on('cosmere-rpg.useItem', (item, _rollConfig, _options) => {
 		case "stormlight":
 		case "surge":
 		case "power":
-			if (IsModuleActive(OFFICIAL_MODULES.handbook)) {
-				cosmereAutomatedActions.handbookMacros[itemId](item, actor);
-			} else {
-				cosmereAutomatedActions.starterRulesMacros[itemId](item, actor);
-			}
-			break;
-		//any case that could be world guide or starter rules
-		case "adversary feature":
-			if (IsModuleActive(OFFICIAL_MODULES.worldguide)) {
-				cosmereAutomatedActions.worldguideMacros[itemId](item, actor);
-			} else {
-				cosmereAutomatedActions.starterRulesMacros[itemId](item, actor);
-			}
-			break;
+		case "adversary feature": {
+			cosmereAutomatedActions.macros[itemId](item, actor);
+		}
 		default: return;
 	}
 })
