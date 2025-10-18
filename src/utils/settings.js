75bcd31810579c1f9edf5a77cbb278281cc94d2d
CyprianStream <163608322+CyprianStream@users.noreply.github.com>
2025-10-17 20:42:28 -0400

    Feat: Added settings page added automations toggle added sphere dunning toggle (currently unused)

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,3 +1,37 @@
+import { MODULE_ID } from "./constants"
+
+export const settings = [
+    {
+        name: "Use Automations",
+        id: "useAutomations",
+        hint: "Toggles CAA automations on or off across all sheets",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true,
+        requiresReload: false,
+    },
+    {
+        name: "Track Dun Spheres",
+        id: "sphereDunning",
+        hint: "Automatically Dun and Infuse spheres as they are used",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true,
+        requiresReload: false
+    }
+]
+
 export function registerModuleSettings(){
-    
+    settings.forEach(setting => {
+        game.settings.register(MODULE_ID, setting.id, setting)
+    });
 }
+
+
+
+
+
+
+
