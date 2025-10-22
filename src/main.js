f11c15ea4753bb41650d863fdb46b8edf8f2f82e
CyprianStream <163608322+CyprianStream@users.noreply.github.com>
2025-10-22 00:58:32 -0400

    Feat: Added Conditions Automations Added Exhausted roll modification Added Exhausted Decrement on long rest Added Empowered roll modification Added Restrained roll modification Added Disoriented roll modification

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { macros } from "./macros/index.js";
 import { registerModuleSettings } from "./utils/settings.js";
 import { nameToId } from "./utils/helpers.js";
 import { MODULE_ID } from "./utils/constants.js";
+import { applyRollConditions, decrementExhausted } from "./automations/conditions.js";
 
 Hooks.once('init', () => {
 	globalThis.cosmereAutomatedActions = {
@@ -23,4 +24,30 @@ Hooks.on('cosmere-rpg.useItem', (item, _rollConfig, _options) => {
 	if(itemId = "new-action"){itemId = nameToId(item.name)}
     const macro = cosmereAutomatedActions.macros[itemId];
     if(macro) macro(item,actor);
+})
+
+Hooks.on('cosmere-rpg.attackRoll', (roll, item, _options) => {
+    if(!game.settings.get(MODULE_ID, "automateConditions")){
+        return
+    }
+    console.log("CAA | Applying Roll Conditions")
+    applyRollConditions(roll, actor)
+})
+
+
+Hooks.on('cosmere-rpg.skillRoll', (roll, actor, _options) => {
+    if(!game.settings.get(MODULE_ID, "automateConditions")){
+        return
+    }
+    console.log("CAA | Applying Roll Conditions")
+    applyRollConditions(roll, actor)
+})
+
+Hooks.on('cosmere-rpg.rest', (actor, length) => {
+	if(!game.settings.get(MODULE_ID, "automateConditions")){
+        return
+    }
+	if(actor.effects.get("condexhausted000") && length === "long"){
+		decrementExhausted(actor)
+	}
 })
