bb67c3f32c30bc78698991398df359ea889ec279
posadist-revolution <ihunickesmith@gmail.com>
2026-02-19 20:38:29 -0700

    Feat/illumination registration move (#88)
    * First pass at re-registering illumination
    
    * Improved layout and added TODO for RAW

--- src/module/macros/surges/illumination/illumination.ts
+++ src/module/macros/surges/illumination/illumination.ts
@@ -1,7 +1,41 @@
 import { CosmereItem, CosmereActor } from "@system/documents";
-import { giveActorItem } from "../../../utils/helpers";
-import { log } from "@module/utils/helpers";
+import { log, giveActorItem, giveActorEffect, deleteDescendantUuids } from "@module/utils/helpers";
+import { MODULE_ID } from "@module/constants";
 
+//#region Effect Create Data
+const disguiseEffectDefaultCreateData: ActiveEffect.CreateData = {
+    "name": "Disguised",
+    "img": "icons/magic/unholy/silhouette-robe-evil-power.webp",
+    "disabled": false,
+    "duration": {
+        "rounds": 1,
+        "startTime": null,
+        "seconds": null,
+        "combat": null,
+        "turns": null,
+        "startRound": null,
+        "startTurn": null
+    },
+    "_id": "nblczTlWrh4Z7uXU",
+    "type": "base",
+    //@ts-ignore
+    "system": {
+        "isStackable": false,
+        "stacks": null
+    },
+    "changes": [],
+    "description": "",
+    "origin": null,
+    "tint": "#ffffff",
+    "transfer": true,
+    "statuses": [],
+    "sort": 0,
+    "flags": {}
+}
+//#endregion
+
+// MACROS
+//#region Macro Functions
 export async function illumination(item: CosmereItem, actor: CosmereActor){
     await foundry.applications.api.DialogV2.wait({
         window: { title: "Illumination" },
@@ -17,40 +51,70 @@ export async function illumination(item: CosmereItem, actor: CosmereActor){
             {
                 label: "Complex Illusion",
                 action: "complex-illusion",
-                callback: async () => {
-                    //adds "Dismiss Complex Illusion" item to actor
-                    const dismissComplexIllusionUUID = "Compendium.cosmere-automated-actions.caaactions.Item.EZqaHREQyTkBiRIb";
-                    const dismissComplexIllusion = await giveActorItem(actor, dismissComplexIllusionUUID);
-                }
+                callback: async () => { createComplexIllusion(item, actor) }
             },
             {
                 label: "Disguise Yourself",
                 action: "disguise-yourself",
-                callback: async () => {
-                    //adds "Dismiss Disguise" item to actor
-                    const dismissDisguiseUUID = "Compendium.cosmere-automated-actions.caaactions.Item.3SdIMgwufXgAfQmM";
-                    const dismissDisguise = await giveActorItem(actor, dismissDisguiseUUID);
-                }
+                callback: async () => { applySelfDisguise(item, actor) }
             }
         ]
     })
 
 }
 
-export function dismissComplexIllusion(item: CosmereItem, actor: CosmereActor){
+export async function cancelComplexIllusion(item: CosmereItem, actor: CosmereActor){
+    deleteDescendantUuids(item.getFlag(MODULE_ID, "descendantUuids"));
     item.delete();
 }
-export function dismissDisguise(item: CosmereItem, actor: CosmereActor){
+export async function cancelDisguise(item: CosmereItem, actor: CosmereActor){
+    deleteDescendantUuids(item.getFlag(MODULE_ID, "descendantUuids"));
     item.delete();
 }
+//#endregion
 
-export function complexIllusionRound(item: CosmereItem, actor: CosmereActor, turn: Combat.HistoryData){
+// START TURN EFFECTS
+//#region Start Turn Effects
+export async function complexIllusionTurnStart(item: CosmereItem, actor: CosmereActor, turn: Combat.HistoryData){
     //Subtracts 1 investiture from actor every combat round
     const actorInv = actor.system.resources.inv.value;
     if(actorInv < 1){
-        dismissComplexIllusion(item, actor);
+        cancelComplexIllusion(item, actor);
         return;
     }
     const newInv = actorInv - 1;
     actor.update({ 'system.resources.inv.value': newInv } as any);
 }
+//#endregion
+
+// INVESTITURE CHANGE EFFECTS
+//#region Inv Change Effects
+
+//TODO: add inv-to-zero to remove self disguise
+
+//#endregion
+
+// HELPERS
+//#region Helpers
+async function applySelfDisguise(item: CosmereItem, actor: CosmereActor){
+    //TODO: This should not spend investiture
+    //adds "Dismiss Disguise" item to actor
+    const dismissDisguiseUUID = "Compendium.cosmere-automated-actions.caaactions.Item.3SdIMgwufXgAfQmM";
+    const dismissDisguise = await giveActorItem(actor, dismissDisguiseUUID);
+
+    //Adds "Gravitation Infusion" item to target
+    var disguiseEffectCreateData = foundry.utils.deepClone(disguiseEffectDefaultCreateData);
+
+    if(dismissDisguise){
+        const disguiseEffect = await giveActorEffect(actor, disguiseEffectCreateData);
+        dismissDisguise.setFlag(MODULE_ID, "descendantUuids", [disguiseEffect?.uuid!]);
+    }
+}
+
+async function createComplexIllusion(item: CosmereItem, actor: CosmereActor){
+    //adds "Dismiss Complex Illusion" item to actor
+    const dismissComplexIllusionUUID = "Compendium.cosmere-automated-actions.caaactions.Item.EZqaHREQyTkBiRIb";
+    const dismissComplexIllusion = await giveActorItem(actor, dismissComplexIllusionUUID);
+}
+
+//#endregion
